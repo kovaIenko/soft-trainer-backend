c6e3be8fe686eca8d25accb7c44fb62c749462c4
kovaIenko <rkovalenko@viax.io>
2024-04-17 23:27:24 +0300

    configs cors #4

--- src/main/java/com/backend/softtrainer/configs/CorsConfig.java
+++ src/main/java/com/backend/softtrainer/configs/CorsConfig.java
@@ -1,35 +1,22 @@
 package com.backend.softtrainer.configs;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Slf4j
 public class CorsConfig {
 
-  @Bean
-  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-//        log.info("CORS domain are: {}", Arrays.asList(corsDomains));
-
-    var source = new UrlBasedCorsConfigurationSource();
-    var config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-//        config.setAllowedOrigins(List.of(corsDomains));
-    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    config.setAllowedHeaders(List.of("*"));
-    config.setExposedHeaders(List.of("*"));
-    source.registerCorsConfiguration("/**", config);
-    var bean = new FilterRegistrationBean<>(new CorsFilter(source));
-    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    return bean;
-  }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("https://test-web-flutter-427fd.web.app/");
+            }
+        };
+    }
 }
