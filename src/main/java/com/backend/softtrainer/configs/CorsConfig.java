dbe83705b845215a22f8d847d4dfe0acf1362b27
kovaIenko <rkovalenko@viax.io>
2024-04-17 23:19:37 +0300

    configs cors #3

--- src/main/java/com/backend/softtrainer/configs/CorsConfig.java
+++ src/main/java/com/backend/softtrainer/configs/CorsConfig.java
@@ -1,23 +1,35 @@
 package com.backend.softtrainer.configs;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
+@Slf4j
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*"); // or specify your allowed origins explicitly
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+//        log.info("CORS domain are: {}", Arrays.asList(corsDomains));
+
+    var source = new UrlBasedCorsConfigurationSource();
+    var config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+//        config.setAllowedOrigins(List.of(corsDomains));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setExposedHeaders(List.of("*"));
+    source.registerCorsConfiguration("/**", config);
+    var bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
+  }
 }
