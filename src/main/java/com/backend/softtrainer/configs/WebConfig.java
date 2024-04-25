f22ba0b31149a28637788b987efe47bdea614033
kovaIenko <rkovalenko@viax.io>
2024-04-25 14:40:26 +0300

    just to save

--- src/main/java/com/backend/softtrainer/configs/WebConfig.java
+++ src/main/java/com/backend/softtrainer/configs/WebConfig.java
@@ -1,6 +1,5 @@
 package com.backend.softtrainer.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,8 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://test-web-flutter-427fd.web.app/");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("https://test-web-flutter-427fd.web.app/");
+//    }
 }
