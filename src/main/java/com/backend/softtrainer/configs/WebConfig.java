cb40f3683e9d2d7510d7e29c1bcd9a9a3eda2b29
kovaIenko <rkovalenko@viax.io>
2024-05-03 01:07:03 +0300

    configure cors for the prod

--- src/main/java/com/backend/softtrainer/configs/WebConfig.java
+++ src/main/java/com/backend/softtrainer/configs/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
       )
-      .allowedOrigins("https://api.thesofttrainer.com");
+      .allowedOrigins("https://app.thesofttrainer.com");
   }
 
 }
