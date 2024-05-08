1a1f4520c8bebfad73730e6d634f8d936da5887a
Pavlo Patalashko <Pavlo.Patalashko@nordstrom.com>
2024-05-08 14:35:41 +0300

    added log watch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# set -e
-
 source .env
 
 ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker stop softtrainer-backend && docker rm softtrainer-backend"
 ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker run -d -p 8443:8443 --name softtrainer-backend ${AWS_ECR_ENDPOINT}/softtrainer-backend:latest"
-# ssh -i "aws-kovalenko98.pem" ec2-user@${EC2_IP} ""
+
+# Check logs on server: ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker logs softtrainer-backend -f"
