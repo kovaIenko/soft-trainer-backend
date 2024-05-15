c692561595db2ee9b3420e7b8c09126034d3d547
kovaIenko <rkovalenko@viax.io>
2024-05-15 22:13:06 +0300

    fix CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,11 @@
 source .env
 
 ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker stop softtrainer-backend && docker rm softtrainer-backend"
+
+ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker rmi $(docker images '${AWS_ECR_ENDPOINT}/softtrainer-backend' -a -q)"
+
+aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ECR_ENDPOINT}"
+
 ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker run -d -p 8443:8443 --name softtrainer-backend ${AWS_ECR_ENDPOINT}/softtrainer-backend:latest"
 
 # Check logs on server: ssh -i "~/.ssh/aws-kovalenko98.pem" ec2-user@${EC2_IP} "docker logs softtrainer-backend -f"
