d999a6032acd9bdfda609c7f399671277bba4f7b
CG Spring Support <spring.cg@outlook.com>
2025-07-06 10:36:37 +0300

    fix the problem of visiblity of skills

--- src/main/java/com/backend/softtrainer/controllers/SkillController.java
+++ src/main/java/com/backend/softtrainer/controllers/SkillController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.backend.softtrainer.services.auth.AuthUtils.userIsOwnerApp;
 import static com.backend.softtrainer.utils.Converter.convertSkills;
@@ -29,51 +30,56 @@ import static com.backend.softtrainer.utils.Converter.convertSkills;
 @Slf4j
 public class SkillController {
 
-    private final SkillService skillService;
+  private final SkillService skillService;
 
-    private final CustomUsrDetailsService customUsrDetailsService;
+  private final CustomUsrDetailsService customUsrDetailsService;
 
-    @Deprecated
-    @GetMapping
-    @PreAuthorize("@customUsrDetailsService.orgHasEmployee(authentication, #organization)")
-    public ResponseEntity<AllSkillsResponseDto> getSkills(@RequestParam(name = "org") String organization,
-                                                          final Authentication authentication) {
-        Set<Skill> skills = new HashSet<>();
-        if ((Objects.isNull(organization) || organization.isEmpty())) {
-            if (userIsOwnerApp(authentication)) {
-                skills = skillService.getAllSkill();
-            } else {
-                return ResponseEntity.ok(new AllSkillsResponseDto(
-                        new HashSet<>(),
-                        new HashSet<>(),
-                        false,
-                        "The organization should be specified for that user"
-                ));
-            }
-        } else {
-            skills = skillService.getAvailableSkillByOrg(organization);
-        }
-
-        var converted = convertSkills(skills);
-        return ResponseEntity.ok(new AllSkillsResponseDto(converted, converted, true, "success"));
+  @Deprecated
+  @GetMapping
+  @PreAuthorize("@customUsrDetailsService.orgHasEmployee(authentication, #organization)")
+  public ResponseEntity<AllSkillsResponseDto> getSkills(@RequestParam(name = "org") String organization,
+                                                        final Authentication authentication) {
+    Set<Skill> skills = new HashSet<>();
+    if ((Objects.isNull(organization) || organization.isEmpty())) {
+      if (userIsOwnerApp(authentication)) {
+        skills = skillService.getAllSkill();
+      } else {
+        return ResponseEntity.ok(new AllSkillsResponseDto(
+          new HashSet<>(),
+          new HashSet<>(),
+          false,
+          "The organization should be specified for that user"
+        ));
+      }
+    } else {
+      skills = skillService.getAvailableSkillByOrg(organization);
     }
 
-    @GetMapping("/available")
-    public ResponseEntity<AllSkillsResponseDto> getSkills(final Authentication authentication) {
-        var username = authentication.getName();
-        var skills = skillService.getAvailableSkill(username);
-        var converted = convertSkills(skills);
-        return ResponseEntity.ok(new AllSkillsResponseDto(converted, converted, true, "success"));
-    }
+    var converted = convertSkills(skills.stream()
+                                    .filter(skill -> !skill.isHidden() && !skill.isAdminHidden()).collect(
+        Collectors.toSet()));
+    return ResponseEntity.ok(new AllSkillsResponseDto(converted, converted, true, "success"));
+  }
 
-    @GetMapping("/simulations")
-    @PreAuthorize("@customUsrDetailsService.isSkillAvailable(authentication, #skillId)")
-    public ResponseEntity<AllSimulationsResponseDto> getAllSimulations(@RequestParam(name = "skillId") Long skillId,
-                                                                       final Authentication authentication) {
-        var userDetails = (CustomUsrDetails) customUsrDetailsService.loadUserByUsername(authentication.getName());
+  @GetMapping("/available")
+  public ResponseEntity<AllSkillsResponseDto> getSkills(final Authentication authentication) {
+    var username = authentication.getName();
+    var skills = skillService.getAvailableSkill(username)
+      .stream()
+      .filter(skill -> !skill.isHidden() && !skill.isAdminHidden())
+      .collect(Collectors.toSet());
+    var converted = convertSkills(skills);
+    return ResponseEntity.ok(new AllSkillsResponseDto(converted, converted, true, "success"));
+  }
 
-        var simulations = skillService.findSimulationsBySkill(userDetails.user(), skillId);
-        return ResponseEntity.ok(new AllSimulationsResponseDto(skillId, simulations, true, "success"));
-    }
+  @GetMapping("/simulations")
+  @PreAuthorize("@customUsrDetailsService.isSkillAvailable(authentication, #skillId)")
+  public ResponseEntity<AllSimulationsResponseDto> getAllSimulations(@RequestParam(name = "skillId") Long skillId,
+                                                                     final Authentication authentication) {
+    var userDetails = (CustomUsrDetails) customUsrDetailsService.loadUserByUsername(authentication.getName());
+
+    var simulations = skillService.findSimulationsBySkill(userDetails.user(), skillId);
+    return ResponseEntity.ok(new AllSimulationsResponseDto(skillId, simulations, true, "success"));
+  }
 
 }
